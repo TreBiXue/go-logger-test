916b65a4462c5c5d07fe1d084af66fd1fbf7aae9
BiXue <10164075bi_xue@cn.tre-inc.com>
2023-05-12 16:09:58 +0900

    go-logger-test

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"go-logger-test/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/questions", func(c *gin.Context) {
+		// 値を取得する
+		questins := model.GetAll()
+		c.JSON(200, questins)
+	})
+	r.GET("/:tag/:num", func(c *gin.Context) {
+		tag := c.Param("tag")
+		num := c.Param("num")
+		question := model.GetBy(tag, num)
+		c.JSON(200, question)
+	})
+
+	r.Run()
+}
